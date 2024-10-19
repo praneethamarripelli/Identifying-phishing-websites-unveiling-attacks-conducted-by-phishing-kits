cd1f68967c1e06cc7aa43f56e8e2a0ea7b157ff5
Praneetha <praneetha1818@gmail.com>
2024-10-19 15:40:05 +0530

    commit

---
+++ PhiKitA/phikita/phikita/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for phikita.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phikita.settings')
+application = get_wsgi_application()
